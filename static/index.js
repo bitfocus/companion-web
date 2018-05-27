95dc8d8485b7341d002e6d7623d5bae0510a1009
William Viker <william@trippelm.no>
2018-05-27 21:47:57 +0200

    website v0.0.1

---
+++ static/index.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	$('#download-location').attr('href', 'https://github.com/bitfocus/companion/releases/')
+	$('#download-location').html("<strong>Mac</strong> companion-osx.dmg");
+
+	$.getJSON( "https://raw.githubusercontent.com/bitfocus/companion/master/package.json", function( data ) {
+		$('#download-location').attr('href', 'https://github.com/bitfocus/companion/releases/tag/v' + data.version)
+		$('#download-location').html("<strong>Mac</strong> companion-v"+data.version+"-beta-osx.dmg");
+	});
+
+});
