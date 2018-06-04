ae9df02817fd3e1cc082078ad70f6ae8e0789292
William Viker <william@trippelm.no>
2018-06-04 12:59:53 +0200

    win&mac

--- static/index.js
+++ static/index.js
@@ -5,7 +5,7 @@ $(function() {
 
 	$.getJSON( "https://raw.githubusercontent.com/bitfocus/companion/master/package.json", function( data ) {
 		$('#download-location').attr('href', 'https://github.com/bitfocus/companion/releases/tag/v' + data.version)
-		$('#download-location').html("<strong>Mac</strong> companion-v"+data.version+"-beta-osx.dmg");
+		$('#download-location').html("<strong>Mac</strong> companion-v"+data.version+" (Mac & Windows)");
 	});
 
 
