d9608211af7a93532e69c705d797a17a8ca27593
William Viker <william@trippelm.no>
2018-06-18 18:09:42 +0200

    website changes for v1.1.0

--- static/index.js
+++ static/index.js
@@ -1,11 +1,11 @@
 $(function() {
 
 	$('#download-location').attr('href', 'https://github.com/bitfocus/companion/releases/')
-	$('#download-location').html("<strong>Mac</strong> companion-osx.dmg");
+	$('#download-location').html("<strong>Companion download</strong>");
 
 	$.getJSON( "https://raw.githubusercontent.com/bitfocus/companion/master/package.json", function( data ) {
 		$('#download-location').attr('href', 'https://github.com/bitfocus/companion/releases/tag/v' + data.version)
-		$('#download-location').html("<strong>Mac</strong> companion-v"+data.version+" (Mac & Windows)");
+		$('#download-location').html("<strong>Companion-v"+data.version+"</strong> (Mac & Windows)");
 	});
 
 
